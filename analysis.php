ea806bc7f01d08721933d33b78963fa2cd758740
Tomas Misura <tmisura@gmail.com>
2025-12-26 16:52:24 +0100

    changed title of webpages

--- analysis.php
+++ analysis.php
@@ -11,7 +11,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Analysis</title>
      <link rel="stylesheet" href="css/style.css?<?php echo time() ?>" />
      <!-- <link rel="stylesheet" href="css/message.css?<?php echo time() ?>" /> -->
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
