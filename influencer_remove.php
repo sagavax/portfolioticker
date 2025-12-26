f9f45525efe00f43e86f3309ec4bf70f8beb9463
Tomas Misura <tmisura@gmail.com>
2025-12-26 16:36:51 +0100

    remove influencer

--- influencer_remove.php
+++ influencer_remove.php
@@ -1,11 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portfolio Ticker - Influencer </title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+
+    include("includes/dbconnect.php");
+    include("includes/functions.php");
+
+
+    $influencer_id = $_POST['influencer_id'];
+
+    $remove_influencer = "DELETE FROM influencers WHERE influencer_id = $influencer_id";
+    $result = mysqli_query($link, $remove_influencer) or die("MySQLi ERROR: ".mysqli_error($link));
+
+?>
